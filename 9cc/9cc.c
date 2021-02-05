3178ada90e968c6e8b4661529f05f54c04891d2a
gongqi.zhen <gongqi.zhen@gmail.com>
2021-02-06 03:49:31 +0900

    Add Token length.

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -20,6 +20,7 @@ struct Token {
   Token *next;    // 次の入力トークン
   int val;        // kindがTK_NUMの場合、その数値
   char *str;      // トークン文字列
+  int len;        // Token length
 };
 
 // Input program
@@ -52,10 +53,11 @@ void error_at(char *loc, char *fmt, ...) {
   exit(1);
 }
 
-// 次のトークンが期待している記号のときには、トークンを１つ読み進めて
-// 真を返す。それ以外の場合には偽を返す。
-bool consume(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
+// Consumes the current token if it matches `op`.
+bool consume(char *op) {
+  if (token->kind != TK_RESERVED ||
+      strlen(op) != token->len ||
+      memcmp(token->str, op, token->len))
     return false;
   token = token->next;
   return true;
--- README.md
+++ README.md
@@ -27,7 +27,7 @@ docker run --rm -it -v $HOME/github/compilerbook/9cc:/9cc -w /9cc compilerbook
 - 文法規則を定義するルールを学ぶ
 - 文法規則を定義するルールをもとに、パーサを書くテクニックを学ぶ
 
-Next:ステップ6：単項プラスと単項マイナス
+Next:新しい文法
 
 目標: 
 まずは、ステップ12までを読み切り、復習して理解を深めたい。：w
